cb9e71c44f39079b7a20a3f7e71c6abc214d1389
Cloud-init-user <debian@django.localdomain>
2018-09-02 21:51:39 +0000

    Add allowed host

--- 42.py
+++ 42.py
@@ -2,13 +2,13 @@ pip3 install django
 PATH="$PATH:/home/sebastian_colomar/.local/bin"
 django-admin.py startproject wisdompets
 cd wisdompets
-python3 manage.py migrate
-python3 manage.py runserver
-sudo apt-get install links
-links http://localhost:8000
-ls -l
 python3 manage.py startapp adoptions
 cat adoptions/models.py
+cat adoptions/admin.py
+grep ALLOWED_HOSTS wisdompets/settings.py
 python3 manage.py makemigrations
 python3 manage.py showmigrations
 python3 manage.py migrate
+python3 manage.py createsuperuser
+python3 manage.py load_pet_data
+python3 manage.py runserver 0.0.0.0:8000
