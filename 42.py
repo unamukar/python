b59c887f7d576787b1d5bbf1a38dc74aba81bf64
Sebastian Colomar <SEBASTIAN.COLOMAR@GMAIL.COM>
2018-09-02 19:27:26 +0200

    Create Django site wisdompets

---
+++ 42.py
@@ -0,0 +1,10 @@
+pip3 install django
+PATH="$PATH:/home/sebastian_colomar/.local/bin"
+django-admin.py startproject wisdompets
+cd wisdompets
+python3 manage.py migrate
+python3 manage.py runserver
+sudo apt-get install links
+links http://localhost:8000
+ls -l
+python3 manage.py startapp adoptions
