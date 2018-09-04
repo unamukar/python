79bca125a002febf938e6798b1f5cc9d4ed95edb
Sebastian Colomar <noboday@nomail.com>
2018-09-04 15:56:05 +0000

    Fix the views and urls configuration files

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -4,8 +4,10 @@ from django.http import Http404
 from .models import Pet
 
 # Create your views here.
-def home(request):return render(request,'home.html',{'pets':pets})
+def home(request):
+    pets=Pet.objects.all()
+    return render(request,'home.html',{'pets':pets})
 def pet_detail(request,id):
-    try:pet=Pet.objects.pet(id=id)
+    try:pet=Pet.objects.get(id=id)
     except Pet.DoesNotExist:raise Http404('Pet not found')
     return render(request,'pet_detail.html',{'pet':pet})
--- wisdompets/wisdompets/urls.py
+++ wisdompets/wisdompets/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
 from adoptions import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('',views.home,name='home'),
-    path('adoptions/(\d+)/',views.pet_detail,name='pet_detail'),
+    re_path('adoptions/(\d+)',views.pet_detail,name='pet_detail'),
 ]
