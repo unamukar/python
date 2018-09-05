ae437adcbe19960df6da5437592c75763ec06511
Sebastian Colomar <noboday@nomail.com>
2018-09-05 12:07:37 +0000

    Correct an error in GET and POST example

--- 44.py
+++ 44.py
@@ -7,13 +7,13 @@ def main():
     datos_get={'profesor':'Sebastian','alumnos':'UTE','metodo':'GET'}
     codigo_get=urllib.parse.urlencode(datos_get)
     resultado_get=urllib.request.urlopen(url_get+'?'+codigo_get)
-    print('Resultado de GET: {}'.format(resultado_get.status))
-    print(resultado_get().decode('utf-8'))
-#    url_post='http://httpbin.org/put'
-#    datos_post={'profesor':'Sebastian','alumnos':'UTE','metodo':'POST'}
-#    codigo_post=urllib.parse.urlencode(datos_post)
-#    resultado_post=urllib.request.urlopen(url_post,data=codigo_post.encode())
-#    print('Resultado de POST: {}'.format(resultado_post.status))
-#    print(resultado_post().decode('utf-8'))
+    print('Resultado de GET: {0}'.format(resultado_get.status))
+    print(resultado_get.read().decode('utf-8'))
+    url_post='http://httpbin.org/post'
+    datos_post={'profesor':'Sebastian','alumnos':'UTE','metodo':'POST'}
+    codigo_post=urllib.parse.urlencode(datos_post)
+    resultado_post=urllib.request.urlopen(url_post,data=codigo_post.encode())
+    print('Resultado de POST: {0}'.format(resultado_post.status))
+    print(resultado_post.read().decode('utf-8'))
 if __name__=='__main__':main()
 
