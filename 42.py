c980a471f0fab4b909d11c53eac6677bd7b8e78b
Sebastian Colomar <noboday@nomail.com>
2018-09-05 11:26:52 +0000

    Clean Django exercise

--- 42.py
+++ 42.py
@@ -1,21 +1,7 @@
 pip3 install django
 PATH="$PATH:/home/sebastian_colomar/.local/bin"
-django-admin.py startproject wisdompets
-cd wisdompets
-python3 manage.py startapp adoptions
-cat adoptions/models.py
-cat adoptions/admin.py
-grep ALLOWED_HOSTS wisdompets/settings.py
-python3 manage.py makemigrations
-python3 manage.py showmigrations
-python3 manage.py migrate
-python3 manage.py createsuperuser
-python3 manage.py load_pet_data
-python3 manage.py runserver 0.0.0.0:8000
 sudo iptables -t nat -A PREROUTING -d 142.93.169.43/32 -i eth0 -p tcp -m tcp --dport 80 -j DNAT --to-destination 142.93.169.43:8000
 sudo iptables -t nat -A POSTROUTING -s 142.93.169.43/32 -o eth0 -p tcp -m tcp --sport 8000 -j MASQUERADE
-
-
 django-admin.py startproject cineteca&&cd cineteca
 python3 manage.py startapp peliculas
 sed -i "/ALLOWED/s/\[\]/\['django.gotdns.org'\]/" cineteca/settings.py
